f570070d9570833364f2a27a64244c2a63a6d889
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-20 13:12:51 +0430

    Add new units to convert

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -6,9 +6,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: number, ...args: unknown[]): unknown {
-    return value *1.6;
-
+  transform(mile: number, targetUnits: string): any {
+    if(!mile){
+       return;
+    }
+    switch(targetUnits){
+      case 'km':
+        return mile * 1.6;
+      case 'm' :
+        return mile * 1.6 * 1000;
+      case 'cm':
+        return mile * 1.6 * 1000 * 1000;
+      default :
+        throw new Error ("Not Suportet Unit : "+ targetUnits)    
+    } 
   }
-
 }
